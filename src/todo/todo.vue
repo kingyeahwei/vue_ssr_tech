9bafb84998e0dca55359df18e514654a8144fee3
kingyeahwei <kingyeahwei@163.com>
2020-08-02 23:41:27 +0800

    完成todo

--- src/todo/todo.vue
+++ src/todo/todo.vue
@@ -7,21 +7,23 @@
       placeholder="接下去要做什么?"
       @keyup.enter="addTodo"
     >
-    <Item :todo="todo"></Item>
-    <Tabs :filter="filter"></Tabs>
+    <Item
+      :todo="todo"
+      v-for="todo in filteredTodos"
+      :key="todo.id"
+      @del="deleteTodo"
+    ></Item>
+    <Tabs :filter="filter" :todos="todos" @toggle="toggleFilter" @clearAllCompleted="clearAllCompleted"></Tabs>
   </section>
 </template>
 <script>
   import Item from './item.vue'
   import Tabs from './tabs.vue'
+  let id = 0;
   export default {
     data() {
       return {
-        todo: {
-          id: 0,
-          content: "this is todo",
-          completed: false
-        },
+        todos: [],
         filter: 'all'
       }
     },
@@ -29,9 +31,32 @@
       Item,
       Tabs
     },
+    computed: {
+      filteredTodos() {
+        if (this.filter == 'all') {
+          return this.todos
+        }
+        const compeleted = this.filter == 'completed'
+        return this.todos.filter(todo => todo.completed == compeleted)
+      }
+    },
     methods: {
-      addTodo() {
-
+      addTodo(e) {
+        this.todos.unshift({
+          id: id++,
+          content: e.target.value.trim(),
+          completed: false
+        })
+        e.target.value = ''
+      },
+      deleteTodo(id) {
+        this.todos.splice(this.todos.findIndex(todo => {todo.id === id}), 1)
+      },
+      toggleFilter(state) {
+        this.filter = state;
+      },
+      clearAllCompleted() {
+        this.todos = this.todos.filter(todo => !todo.completed)
       }
     }
   }
